ddc1e307ff3d6b4852bfdbc9b5b4d7d48c57fcdb
hjmr <2620990+hjmr@users.noreply.github.com>
2024-05-28 19:13:50 +0900

    update example.

--- test.py
+++ test.py
@@ -1,19 +1,20 @@
 import time
 from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.common.by import By
 
+options = webdriver.ChromeOptions()
 driver = webdriver.Remote(
     command_executor="http://localhost:4444/wd/hub",
-    desired_capabilities=DesiredCapabilities.CHROME)
+    options=options)
 print("remote")
 driver.get("https://www.google.com/")
 time.sleep(5)
-search_box = driver.find_element_by_name("q")
+search_box = driver.find_element(By.NAME, "q")
 search_box.send_keys('M3 Mac')
 search_box.submit()
 time.sleep(5)
 driver.save_screenshot("google_search_screenshot.png")
-results = driver.find_elements_by_xpath("//div[@id='search']/*/*/div[@class='g']/div/div/div/a")
+results = driver.find_elements(By.XPATH, '//div[@class="g"]/*/*/*/*/*/*/h3')
 for element in results:
-    print(element.text)
+    print(element.tag_name, element.text)
 driver.quit()
